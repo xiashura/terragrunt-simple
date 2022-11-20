424ce6ed0cd7f5bc3cf8677f9d4ae35b65b9568a
xiashura <plexanov@protonmail.com>
2022-11-20 16:00:34 +0300

    add develop shell

---
+++ default.nix
@@ -0,0 +1,26 @@
+
+with (import <nixpkgs> {});
+
+let
+ init = pkgs.writeShellScriptBin "init" ''
+  docker-compose up -d 
+  docker exec -it localstack_main awslocal s3api create-bucket \
+   --no-sign-request --bucket my-bucket --region us-east-1
+  '';
+
+in
+
+ 
+
+stdenv.mkDerivation {
+
+
+  name = "aws-local-terraform";
+  buildInputs = [
+    init
+    terraform
+    terragrunt
+    awscli2
+    jq
+  ];
+}
